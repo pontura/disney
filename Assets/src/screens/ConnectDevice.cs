2b438d48b6d943b70592d08cd5aed6828b9d6716
pontura <pontura@gmail.com>
2016-04-29 11:01:04 -0300

    botones que faltaban y otros

---
+++ Assets/src/screens/ConnectDevice.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectDevice : ScreenMain {
+
+    public void Back()
+    {
+        Events.Back();
+    }
+    public void Connect()
+    {
+
+    }
+}
---
+++ Assets/src/ui/UserButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UserButton : MonoBehaviour {
+
+    public Text field;
+
+    public Image boy;
+    public Image girl;
+
+    [HideInInspector]
+    public UserData userData;
+
+    private Users users;
+
+    public void Init(Users users, UserData userData)
+    {
+        this.userData = userData;
+        this.users = users;
+        field.text = userData.username;
+        if (userData.sex == UserData.sexs.BOY)
+        {
+            boy.enabled = true;
+            girl.enabled = false;
+        }
+        else
+        {
+            boy.enabled = false;
+            girl.enabled = true;
+        }
+	}
+    public void Clicked()
+    {
+        users.Ready(this);
+    }
+    public void Delete()
+    {
+        users.Delete(this);
+    }
+    public void Edit()
+    {
+        users.Edit(this);
+    }
+}
