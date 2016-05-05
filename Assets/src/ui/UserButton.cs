1ec5d6f6d4a414c9ec186c6285e36b650ab40ee7
pontura <pontura@gmail.com>
2016-05-05 14:18:06 -0300

    boton de user edit con "swipe inteligente" by LOLO

--- Assets/src/ui/UserButton.cs
+++ Assets/src/ui/UserButton.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.EventSystems;
 
-public class UserButton : MonoBehaviour {
+public class UserButton : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler {
 
     public Text field;
 
@@ -14,8 +15,22 @@ public class UserButton : MonoBehaviour {
 
     private Users users;
 
+	private bool open;
+	private int MoveX;
+	float	drag_x;
+	bool	dragging;
+
+	public void Start()
+	{
+		MoveX = (int)(70 * ScreenManager.Instance.canvas.scaleFactor);
+		open = true;
+		dragging = false;
+	}
+
     public void Init(Users users, UserData userData)
     {
+		open = true;
+		dragging = false;
         this.userData = userData;
         this.users = users;
         field.text = userData.username;
@@ -42,4 +57,47 @@ public class UserButton : MonoBehaviour {
     {
         users.Edit(this);
     }
+
+	//	edit
+
+	private void Toogle()
+	{
+		int dest = open ? MoveX : -MoveX;
+
+		iTween.MoveBy(gameObject, iTween.Hash(
+			"x", dest,
+			"time", .2,
+			"easeType", "easeOutCubic",
+			"oncomplete", "OnAnimationComplete"
+		));
+	}
+
+	public void OnBeginDrag(PointerEventData _EventData)
+	{
+		drag_x = _EventData.position.x;
+	}
+
+	public void OnDrag(PointerEventData _EventData)
+	{
+		if (!dragging) {
+			dragging = false;
+			if (_EventData.position.x > drag_x) {
+				if (!open) {
+					open = true;
+					Toogle ();
+				}
+			} else if(_EventData.position.x < drag_x){
+				if (open) {
+					open = false;
+					Toogle ();
+				}
+			}
+		}
+	}
+
+	public void OnEndDrag(PointerEventData _EventData)
+	{
+		dragging = false;
+	}
+
 }
